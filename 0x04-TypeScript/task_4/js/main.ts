ed8003dd93335b35b2c070927225c79d195f700a
OgborFaith <faithogbor23@gmail.com>
2025-11-09 13:02:12 +0100

    Create and export subjects and Teacher, call methods for Cpp, Java, React

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,28 @@
+
+export const cTeacher: Subjects.Teacher = {
+  firstName: "John",
+  lastName: "Doe",
+  experienceTeachingC: 10,
+};
+
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+// C++ Subject
+console.log("C++");
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+// Java Subject
+console.log("Java");
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// React Subject
+console.log("React");
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
