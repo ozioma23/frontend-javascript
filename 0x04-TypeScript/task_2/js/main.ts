d6d3e990d79bb855b18fb70aef9d832106f8372a
OgborFaith <faithogbor23@gmail.com>
2025-11-09 10:40:51 +0100

    Add isDirector and executeWork functions

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,10 @@
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return employee.workTeacherTasks();
+}
